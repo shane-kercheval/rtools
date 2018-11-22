5c02c8df7a47b87bafea0ece7e1f69a1b2553133
Shane Kercheval <shane.kercheval@gmail.com>
2018-11-21 18:29:48 -0800

    added `rt_explore_correlations` & `rt_explore_plot_correlations`

--- R/explore_basics.R
+++ R/explore_basics.R
@@ -33,7 +33,8 @@ rt_explore_numeric_summary <- function(dataset) {
 
     dataset <- dataset %>% select_if(is.numeric)
 
-    return (data.frame(
+    results <- data.frame(
+        feature=colnames(dataset),
         non_nulls=apply(dataset, 2, function(x) sum(!is.na(x))),
         nulls=apply(dataset, 2, function(x) sum(is.na(x))),
         perc_nulls=apply(dataset, 2, function(x) round(sum(is.na(x)) / nrow(dataset), 4)),
@@ -51,7 +52,10 @@ rt_explore_numeric_summary <- function(dataset) {
         percentile_75=apply(dataset, 2, function(x) quantile(x, .75, na.rm=TRUE)),
         percentile_90=apply(dataset, 2, function(x) quantile(x, .90, na.rm=TRUE)),
         max=apply(dataset, 2, function(x) max(x, na.rm=TRUE)))
-    )
+
+    rownames(results) <- NULL
+
+    return (results)
 }
 
 #' returns a dataframe containing summary statistics for categoric columns passsed to `dataset`
@@ -70,13 +74,106 @@ rt_explore_categoric_summary <- function(dataset) {
 
     dataset <- dataset %>% select_if(function(x) !is.numeric(x))
 
-    return (data.frame(
+    results <- data.frame(
+        feature=colnames(dataset),
         non_nulls=apply(dataset, 2, function(x) sum(!is.na(x))),
         nulls=apply(dataset, 2, function(x) sum(is.na(x))),
         perc_nulls=apply(dataset, 2, function(x) round(sum(is.na(x)) / nrow(dataset), 4)),
         top=apply(dataset, 2, function(x) as.character(value_counts(x)[1, 'values'])),
         unique=apply(dataset, 2, function(x) nrow(value_counts(x))),
-        perc_unique=apply(dataset, 2, function(x) nrow(value_counts(x)) / nrow(dataset))
-        )
-    )
+        perc_unique=apply(dataset, 2, function(x) nrow(value_counts(x)) / nrow(dataset)))
+
+
+    rownames(results) <- NULL
+
+    return (results)
+}
+
+#' returns a matrix containing of correlations
+#'
+#' @param dataset dataframe containing numberic columns
+#' @param corr_threshold any correlations that are <= `corr_threshold` will be set to `NA`. (Default is `0`, so all correlations are shown). Helps to reduce noise.
+#' @param p_value_threshold any correlations that have a p-value greater than `p_value_threshold` will be set to `NA` (Default is `1`, so all correlations are shown)
+#' @param type type of correlation to perform (Default is `pearson`)
+#'
+#' @examples
+#'
+#' library(ggplot2)
+#' rt_explore_correlations(iris)
+#'
+#' @importFrom magrittr "%>%"
+#' @importFrom dplyr select_if
+#' @importFrom Hmisc rcorr
+#' @importFrom stats complete.cases
+#' @export
+rt_explore_correlations <- function(dataset, corr_threshold=0, p_value_threshold=1, type='pearson') {
+
+    data_numeric = dataset %>% select_if(is.numeric)
+
+    rcorr_results = rcorr(as.matrix(data_numeric[complete.cases(data_numeric), ]), type=type)
+    correlations = rcorr_results$r
+    p_values = rcorr_results$P
+
+    correlations[which(abs(correlations) <= corr_threshold, arr.ind=TRUE)] <- NA # set correlations that are 'lower' than corr_threshold to NA
+    correlations[which(p_values > p_value_threshold, arr.ind=TRUE)] <- NA # set correlations that have p_value > `p_value_threshold` to NA (i.e. we only want values who have low p_value (i.e. statistically significant), lower than pvalue threshold)
+
+    # set diagnal and above to NA (it is just duplicated information)
+    correlations[upper.tri(correlations, diag = TRUE)] <- NA
+
+    # the first row and the last column will contain ALL NAs, remove
+    correlations <- correlations[-1,]
+    correlations <- correlations[,-ncol(correlations)]
+
+    return (as.matrix(round(correlations, 2)))
+}
+
+#' returns a heatmap of correlations
+#'
+#' @param dataset dataframe containing numberic columns
+#' @param corr_threshold any correlations that are <= `corr_threshold` will be set to `NA`. (Default is `0`, so all correlations are shown). Helps to reduce noise.
+#' @param p_value_threshold any correlations that have a p-value greater than `p_value_threshold` will be set to `NA` (Default is `1`, so all correlations are shown)
+#' @param type type of correlation to perform (Default is `pearson`)
+#'
+#' @examples
+#'
+#' library(ggplot2)
+#' rt_explore_correlations(iris)
+#'
+#' @importFrom magrittr "%>%"
+#' @importFrom dplyr filter mutate
+#' @importFrom reshape2 melt
+#' @importFrom ggplot2 ggplot aes geom_tile geom_text scale_fill_gradientn labs theme element_rect element_text
+#' @export
+rt_explore_plot_correlations <- function(dataset, corr_threshold=0, p_value_threshold=0.1, type='pearson') {
+
+    correlations <- rt_explore_correlations(dataset=dataset,
+                                            corr_threshold=corr_threshold,
+                                            p_value_threshold=p_value_threshold,
+                                            type=type)
+
+    column_names <- colnames(correlations)
+    row_names <- rownames(correlations)
+
+    melted_correlations <- melt(correlations) %>%
+        filter(!is.na(value)) %>%
+        mutate(Var1 = factor(Var1, levels = rev(row_names)),
+               Var2 = factor(Var2, levels = column_names))
+
+    ggplot(melted_correlations, aes(x=Var2, y=Var1)) +
+        geom_tile(aes(fill = value)) +
+        geom_text(aes(label = value)) +
+        scale_fill_gradientn(colours=c('blue','white','red'),
+                             breaks=c(-1, 0, 1),
+                             labels=c("Perfect Neg Correlation", "No Correlation", "Perfect Pos Correlation"),
+                             limits=c(-1,1)) +
+        labs(title = 'Correlations',
+             subtitle = paste0('corr_threshold: ', corr_threshold, '; ',
+                               'p_value_threshold: ', p_value_threshold, '; ',
+                               'type: ', type),
+             x = '',
+             y = '',
+             fill = 'Colors') +
+        theme(panel.background = element_rect(fill = 'white', colour = 'white'),
+              axis.text.x = element_text(angle = 30, hjust = 1))
+
 }
--- tests/testthat/test_explore.R
+++ tests/testthat/test_explore.R
@@ -1,17 +1,20 @@
 context('Exploratory Analysis')
+library(testthat)
 library(ggplot2)
 
 test_that("rt_explore_numeric_summary", {
-    iris[1, 'Sepal.Width'] <- NA
-    iris[2, 'Sepal.Width'] <- NA
-    iris[3, 'Sepal.Width'] <- NA
-    iris[1, 'Petal.Length'] <- NA
-    iris[1, 'Sepal.Length'] <- 0
-    iris[2, 'Sepal.Length'] <- 0
-    iris[3, 'Sepal.Length'] <- 0
-    iris[1, 'Petal.Width'] <- 0
 
-    result <- rt_explore_numeric_summary(dataset=iris)
+    temp_iris <- iris
+    temp_iris[1, 'Sepal.Width'] <- NA
+    temp_iris[2, 'Sepal.Width'] <- NA
+    temp_iris[3, 'Sepal.Width'] <- NA
+    temp_iris[1, 'Petal.Length'] <- NA
+    temp_iris[1, 'Sepal.Length'] <- 0
+    temp_iris[2, 'Sepal.Length'] <- 0
+    temp_iris[3, 'Sepal.Length'] <- 0
+    temp_iris[1, 'Petal.Width'] <- 0
+
+    result <- rt_explore_numeric_summary(dataset=temp_iris)
 
     rds_file <- 'data/rt_explore_numeric_summary_iris.RDS'
     expect_true(rt_are_dataframes_equal_from_file(dataframe1=result, rds_file=rds_file))
@@ -19,12 +22,43 @@ test_that("rt_explore_numeric_summary", {
 
 test_that("rt_explore_categoric_summary", {
 
-    iris[1, 'Species'] <- NA
-    iris[2, 'Species'] <- NA
-    iris[3, 'Species'] <- NA
+    temp_iris <- iris
+    temp_iris[1, 'Species'] <- NA
+    temp_iris[2, 'Species'] <- NA
+    temp_iris[3, 'Species'] <- NA
 
-    result <- rt_explore_categoric_summary(dataset=iris)
+    result <- rt_explore_categoric_summary(dataset=temp_iris)
 
     rds_file <- 'data/rt_explore_categoric_summary_iris.RDS'
     expect_true(rt_are_dataframes_equal_from_file(dataframe1=result, rds_file=rds_file))
+})
+
+test_that("rt_explore_correlations_credit", {
+    credit_data <- read.csv("data/credit.csv", header=TRUE)
+    # make sure it handles NAs
+    credit_data[1, 'months_loan_duration'] <- NA
+
+    correlations <- rt_explore_correlations(dataset=credit_data,
+                                            corr_threshold=0,
+                                            p_value_threshold=1,
+                                            type='pearson')
+
+    rds_file <- 'data/rt_correlations_credit.RDS'
+    expect_true(rt_are_dataframes_equal_from_file(dataframe1=data.frame(correlations), rds_file=rds_file))
+
+    correlation_plot_file <- 'data/rt_explore_plot_correlations_credit.png'
+    if (file.exists(correlation_plot_file)) file.remove(correlation_plot_file)
+    ggsave(filename=correlation_plot_file,
+           plot=rt_explore_plot_correlations(dataset=credit_data,
+                                             corr_threshold=0,
+                                             p_value_threshold=1,
+                                             type='pearson'))
+
+    expect_true(file.exists(correlation_plot_file))
+
+
+
+    # todo: test different parameters
+
+
 })
