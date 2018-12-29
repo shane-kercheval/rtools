61cf840e450dd837cdd812515bf19c6c523c1193
Shane Kercheval <shane.kercheval@gmail.com>
2018-12-29 13:26:45 -0800

    added `rt_ts_lm_build_formula`

--- R/times_series.R
+++ R/times_series.R
@@ -26,3 +26,81 @@ rt_ts_is_multi_variable <- function(x) {
     return (any(classes == 'mts') || any(classes == 'msts'))
 }
 
+#' returns a forumla string to pass `tslm()``
+#'
+#' @param dependent_variable name of the depedent variable in the dataset
+#' @param independent_variables name of the indepedent variables in the dataset (including "trend" & "season" if applicable)
+#' @param interaction_variables not supported yet
+#' @param ex_ante_horizon is used when the regression model will be used to forecast future values, `ex_ante_horizon` specifies the number periods (i.e. horizon) that will be forecasted.
+#'
+#' Ex ante means the forecast will be a "true" forecast and, in this case, will only use lagged values that are lagged far enough back, that we can use them to predict into the future. (and/or trend/season variables)
+#' For example, if we want to lag 4 periods (e.g. quarters) ahead, we must use values that are lagged 4 periods behind.
+#'
+#' Lagged variables must be in the for of `x_lag_y`, where `x` is the original variable name and `y` is the lag number.
+#'
+#' If `ex_ante_horizon` is set, only variables that are in the form of `x_lag_y` will be included, and additionally, only lagged variables where `y` >= `ex_ante_horizon` will be included.
+#'
+#' All independent variables are included of `ex_ante_horizon` is `NULL`
+#' @examples
+#'
+#' rt_ts_lm_build_formula(dependent_variable = 'dataset',
+#'                        independent_variables = c('trend', 'season'))
+#'
+#' @importFrom purrr map_chr
+#' @importFrom stringr str_split
+#' @export
+rt_ts_lm_build_formula <- function(dependent_variable,
+                                   independent_variables=NULL,
+                                   interaction_variables=NULL,
+                                   ex_ante_horizon=NULL) {
+    # ex_ante_horizon specifies the number periods (i.e. horizon) we will be forecasting
+    # this ex_ante (i.e. true forecast) implementation requires the lags included in the regression model
+    # to be >= the ex_ante_horizon.... e.g. cannot have lag 5 included if we are predicting 10 periods ahead, or lag 9
+    # all independent variables are included of ex_ante_horizon is NULL
+
+    if(!is.null(ex_ante_horizon)) {
+
+        #independent_variables <- c('trend', 'season', 'x_lag_1', 'x_lag_2', 'x_lag_3', 'x_lag_4', 'x_lag_5')
+        splits <- str_split(independent_variables, '_lag_', simplify=TRUE)
+        if(ncol(splits) == 1) { # no matches/lags found
+
+            valid_lags <- rep(FALSE, length(independent_variables))  # all invalid
+
+        } else {
+
+            lag_number <- as.numeric(splits[, 2])
+            lag_number[is.na(lag_number)] <- 0  # NA means not a lag
+            valid_lags <- lag_number >= ex_ante_horizon
+        }
+
+        keep_vars <- independent_variables %in% c('trend', 'season') | valid_lags
+        independent_variables <- independent_variables[keep_vars]
+    }
+
+    if(is.null(interaction_variables)) {
+
+        interaction_variables_formula <- ''
+
+    } else {
+
+        interaction_variables_formula <- paste(map_chr(interaction_variables, ~ paste(., collapse =' * ')),
+                                               collapse = ' + ')
+    }
+
+    if(is.null(independent_variables) || length(independent_variables) == 0) {
+
+        independent_variables_formula <- interaction_variables_formula
+
+    } else if(is.null(interaction_variables) || length(interaction_variables) == 0) {
+
+        independent_variables_formula <- paste(independent_variables, collapse =' + ')
+
+    } else {
+
+        independent_variables_formula <- paste(interaction_variables_formula,
+                                               '+',
+                                               paste(independent_variables, collapse =' + '))
+    }
+
+    return (paste(dependent_variable, '~', independent_variables_formula))
+}
--- tests/testthat/test_time_series.R
+++ tests/testthat/test_time_series.R
@@ -19,3 +19,87 @@ test_that('rt_ts_is_multi_variable', {
     expect_true(rt_ts_is_multi_variable(elecdemand))   # classes: msts ts
 })
 
+test_that('rt_ts_lm_build_formula', {
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'dataset',
+                                          independent_variables = c('trend', 'season'),
+                                          ex_ante_horizon=NULL)
+    expect_equal(reg_formula, 'dataset ~ trend + season')
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'dataset',
+                                          independent_variables = c('trend', 'season'),
+                                          ex_ante_horizon=3)
+    expect_equal(reg_formula, 'dataset ~ trend + season')
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'dataset',
+                                          independent_variables = c('trend'),
+                                          ex_ante_horizon=NULL)
+    expect_equal(reg_formula, 'dataset ~ trend')
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'dataset',
+                                          independent_variables = c('trend'),
+                                          ex_ante_horizon=3)
+    expect_equal(reg_formula, 'dataset ~ trend')
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'dataset',
+                                          independent_variables = c('season'),
+                                          ex_ante_horizon=NULL)
+    expect_equal(reg_formula, 'dataset ~ season')
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'dataset',
+                                          independent_variables = c('season'),
+                                          ex_ante_horizon=3)
+    expect_equal(reg_formula, 'dataset ~ season')
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'NSWMetro',
+                                          independent_variables = c('trend', 'season', 'NSWNthCo'))
+    expect_equal(reg_formula, 'NSWMetro ~ trend + season + NSWNthCo')
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'NSWMetro',
+                                          independent_variables = c('trend', 'season', 'NSWNthCo',
+                                                                    'NSWNthCo_lag_1', 'NSWNthCo_lag_2'),
+                                          ex_ante_horizon = NULL)
+    expect_equal(reg_formula, 'NSWMetro ~ trend + season + NSWNthCo + NSWNthCo_lag_1 + NSWNthCo_lag_2')
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'NSWMetro',
+                                          independent_variables = c('trend', 'season', 'NSWNthCo',
+                                                                    'NSWNthCo_lag_1', 'NSWNthCo_lag_2'),
+                                          ex_ante_horizon = 1)
+    expect_equal(reg_formula, 'NSWMetro ~ trend + season + NSWNthCo_lag_1 + NSWNthCo_lag_2')
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'NSWMetro',
+                                          independent_variables = c('trend', 'season', 'NSWNthCo',
+                                                                    'NSWNthCo_lag_1', 'NSWNthCo_lag_2'),
+                                          ex_ante_horizon = 2)
+    expect_equal(reg_formula, 'NSWMetro ~ trend + season + NSWNthCo_lag_2')
+
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'NSWMetro',
+                                          independent_variables = c('trend', 'NSWNthCo',
+                                                                    'NSWNthCo_lag_1', 'NSWNthCo_lag_2',
+                                                                    'XYZ', 'XYZ_lag_1', 'XYZ_lag_2',
+                                                                    'XYZ_lag_3', 'XYZ_lagg_3'),
+                                          ex_ante_horizon = 2)
+    expect_equal(reg_formula, 'NSWMetro ~ trend + NSWNthCo_lag_2 + XYZ_lag_2 + XYZ_lag_3')
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'NSWMetro',
+                                          independent_variables = c('trend', 'NSWNthCo',
+                                                                    'NSWNthCo_lag_1', 'NSWNthCo_lag_2',
+                                                                    'XYZ', 'XYZ_lag_1', 'XYZ_lag_2',
+                                                                    'XYZ_lag_3', 'XYZ_lagg_3'),
+                                          ex_ante_horizon = NULL)
+    expect_equal(reg_formula, 'NSWMetro ~ trend + NSWNthCo + NSWNthCo_lag_1 + NSWNthCo_lag_2 + XYZ + XYZ_lag_1 + XYZ_lag_2 + XYZ_lag_3 + XYZ_lagg_3')
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'NSWMetro',
+                                          independent_variables = c('season', 'NSWNthCo',
+                                                                    'NSWNthCo_lag_1', 'NSWNthCo_lag_2'),
+                                          ex_ante_horizon = 2)
+    expect_equal(reg_formula, 'NSWMetro ~ season + NSWNthCo_lag_2')
+
+    reg_formula <- rt_ts_lm_build_formula(dependent_variable = 'NSWMetro',
+                                          independent_variables = c('NSWNthCo', 'NSWNthCo_lag_1',
+                                                                    'NSWNthCo_lag_2'),
+                                          ex_ante_horizon = 2)
+    expect_equal(reg_formula, 'NSWMetro ~ NSWNthCo_lag_2')
+
+})
