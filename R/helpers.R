dd0a7d8b76a829439e8dff40a0414e6861fa9aef
Shane Kercheval <shane.kercheval@gmail.com>
2018-12-30 10:58:55 -0800

    changed the size of the image of the ggplot that the unit tests save

--- R/helpers.R
+++ R/helpers.R
@@ -29,12 +29,13 @@ rt_are_dataframes_equal_from_file <- function(dataframe1, rds_file) {
 #'
 #' @param file_name path to save plot
 #' @param plot plot to save
+#' @param size_inches c(height, width) in inches
 #'
 #' @importFrom ggplot2 ggsave
 #' @export
-test_save_plot <- function(file_name, plot) {
+test_save_plot <- function(file_name, plot, size_inches=c(5, 8)) {
 
     if (file.exists(file_name)) file.remove(file_name)
-    ggsave(filename=file_name, plot=plot)
+    ggsave(filename=file_name, plot=plot, height=size_inches[1], width=size_inches[2], units='in')
     expect_true(file.exists(file_name))
 }
