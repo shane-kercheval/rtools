4bb6c2231e495d5f180fd6897b8852925e791118
Shane Kercheval <shane.kercheval@gmail.com>
2020-03-07 10:52:15 -0800

    Update test_helpers.R

--- tests/testthat/test_helpers.R
+++ tests/testthat/test_helpers.R
@@ -1,3 +1,16 @@
 test_helper__column_names <- function(.df) {
     return (paste(rt_pretty_text(colnames(.df)), 'Col'))
 }
+
+test_helper_transform_campaign_data <- function(.campaign_data) {
+    .campaign_data %>%
+        select(-interaction) %>%
+        mutate(step_type = ifelse(channel == 'Facebook' | channel == 'Instagram', "Social", "Other")) %>%
+        rename(id=cookie,
+               timestamp=time,
+               step=channel,
+               num_conversions = conversion) %>%
+        select(id, timestamp, step, step_type, num_conversions, conversion_value) %>%
+        arrange(id, timestamp)
+}
+
